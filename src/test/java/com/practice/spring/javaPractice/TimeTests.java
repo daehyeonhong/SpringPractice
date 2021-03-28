2bd390b0ab9ffe2bdefabc1b7623157edd164d81
DaeHyeonHong <fholng@outlook.com>
2021-03-28 21:10:23 +0900

    time Test

--- src/test/java/com/practice/spring/javaPractice/TimeTests.java
+++ src/test/java/com/practice/spring/javaPractice/TimeTests.java
@@ -2,6 +2,10 @@ package com.practice.spring.javaPractice;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.Test;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,10 +14,20 @@ public class TimeTests {
 
     @Test
     public void dateValidateTest() {
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now();
-        LocalTime validateTime = LocalTime.of(23, 30);
-        log.info("{}, {}, {}, {}", localDate, localTime, validateTime, localTime.isAfter(validateTime));
+        Map<String, Object> timeMap = new HashMap<String, Object>();
+
+        LocalDate currDate = LocalDate.now();
+        LocalTime currTime = LocalTime.now();
+        LocalTime baseTime = LocalTime.of(23, 30);
+        Long currDateNumber = Long.valueOf(currDate.format(DateTimeFormatter.BASIC_ISO_DATE));
+        log.info("{}", currDate.format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        boolean delvAble = currTime.isBefore(baseTime);
+        timeMap.put("currDate", currDate);
+        timeMap.put("delvAble", delvAble);
+        timeMap.put("currDateNumber", currDateNumber);
+        log.info("{}", timeMap);
+        log.info("seoul : {}, hongkong : {}", LocalTime.now(ZoneId.of("Asia/Seoul")),
+                LocalTime.now(ZoneId.of("Asia/Hong_Kong")));
     }
 
 }
